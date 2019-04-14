c901eb186b13230ee2e4fbb85fdd152fd7834d47
Jose Seraquive <jose.seraquive@gmail.com>
2019-04-14 03:49:34 -0500

    Adding the test module

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      treeInsert,
+      BinaryTree (Node, Leaf)
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "Jose Seraquive"
+
+
+data BinaryTree a = Node a (BinaryTree a) (BinaryTree a)
+                  |Leaf
+                  deriving (Show, Eq)
+treeInsert :: ( Ord a) => a -> BinaryTree a -> BinaryTree a
+treeInsert x Leaf = Node x Leaf Leaf
+treeInsert x (Node y l r) | (x <= y) = Node y (treeInsert x l) r
+                          | otherwise = Node y l (treeInsert x r )
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit as HU
+import Lib
+
+hunitTestInsertOnLeaf :: TestTree
+hunitTestInsertOnLeaf = HU.testCase "Insert 'a' on empty tree" $
+  assertEqual "Insertion is wrong" (treeInsert 'a' Leaf)(Node 'a' Leaf Leaf)
+
+allTests :: TestTree
+allTests = testGroup "Tasty Test" [
+  testGroup "Hunit Test" [hunitTestInsertOnLeaf]
+ ]
+
+main :: IO()
+main = defaultMain allTests
