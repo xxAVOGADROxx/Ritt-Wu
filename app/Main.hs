adb9113ac157d45a347965ad85a6d86735d8ef05
Jose Seraquive <jose.seraquive@gmail.com>
2019-05-04 11:34:19 -0500

    Monomomial implementation with massiv

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,12 @@
 module Main where
-import Polynomial.Monomial
+--import Polynomial.Monomial
+import Data.Massiv.Array as A
+import Data.Char.SScript
+
+
+
 
 main :: IO ()
-main = someFunc
+main = putStrLn $  formatSS  "(x_{12} - x_{21})^{25} + (y_1 - y_2)^2"
+
+
