98cffd941dbefcc0f38e85b01c3e8dabd4ab50fc
Jose Seraquive <jose.seraquive@gmail.com>
2019-09-05 04:14:04 -0500

    added corrrections

--- src/Polynomial/Polynomial.hs
+++ src/Polynomial/Polynomial.hs
@@ -33,7 +33,7 @@ max' (x:x':xs) =
      xs)
 
 --------------------------------------------------------------
--- leading variable
+-- leading variable or leading term
 lv :: Poly t ord -> Int
 lv = class'
 --------------------------------------------------------------
@@ -53,33 +53,35 @@ md :: Poly t ord -> [Int]
 md = undefined
 
 --------------------------------------------------------------
--- leading monomial
-lt :: (Eq t) => Poly t Lex -> Mon t Lex
-lt xp = ltMax' $ [ x | x <- (getP xp), ld xp == f x ]
+-- leading monomial, completo monomial
+lm :: (Eq t) => Poly t Lex -> Mon t Lex
+lm xp = lmMax' $ [ x | x <- (getP xp), ld xp == f x ]
   where
     f = last . A.toList . getExp . snd . getMon
 
-ltMax' :: (Eq t )=>[Mon t Lex] -> Mon t Lex
-ltMax' [x] = x
-ltMax' (x:x':xs) = ltMax'
+lmMax' :: (Eq t )=>[Mon t Lex] -> Mon t Lex
+lmMax' [x] = x
+lmMax' (x:x':xs) = lmMax'
     ((if x >= x'
         then x
         else x') :
      xs)
 
 --------------------------------------------------------------
--- leading coefficient
-lc :: (Eq t) => Poly t Lex -> t
-lc xp = fst (getMon $ lt xp)
---------------------------------------------------------------
--- leading monomial
-lm :: (Eq t) => Poly t Lex -> Exp Lex
-lm xp = snd (getMon $ lt xp)
+-- leading coefficient, is all the terms without the leading variable 
+lc :: (Eq t)=>Poly t Lex -> Mon t Lex
+lc xp =   lc' $ getMon $ lm xp
+
+lc' :: (k, Exp ord ) -> Mon k ord
+lc' (s, xs) = Mon $ (s,  m . init . toList . getExp $ xs)
 --------------------------------------------------------------
 -- pseudo remainder
-srem :: Poly t ord -> Poly t ord -> Poly t ord
-srem =  undefined
-
+-- srem :: Poly t ord -> Poly t ord -> Poly t ord
+-- srem f g
+--   | m >= k =
+--   | otherwise = undefined
+--   where
+--     m = 
 
 
 scheduleSums :: IO [Int]
@@ -94,3 +96,6 @@ scheduleSums =
 --  Poly [Mon(4,m[1,2]), Mon(-3,m[5,6]), Mon(6,mp[4][9])] :: Poly Int Lex
 -- Poly [Mon(4,m[1,2]), Mon(-3,m[5,10]), Mon(6,mp[4][9]), Mon(15, mp[1,3][3,9]), Mon(30, mp[3,4,5][1,2,3]), Mon(34, mp[3,4,5][2,2,3])] :: Poly Int Lex
 -- a = Poly [Mon(4,m[1,2]), Mon(-3,m[5,10]), Mon(6,mp[4][9]), Mon(15, mp[1,3][3,9]), Mon(30, mp[2,3,4,5][4,1,2,3]), Mon(34, mp[2,3,4,5][8,2,2,3])] :: Poly Int Lex
+
+-- f = Poly [Mon(1,m[2,3]), Mon(-1,m[0,1])] :: Poly Int Lex
+-- g = Poly [Mon(1,m[3,1]), Mon(-2,m[])] :: Poly Int Lex
