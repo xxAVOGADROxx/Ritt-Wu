c9c648439a3fb1539740fcfddfafcffb28ce8185
Jose Seraquive <jose.seraquive@gmail.com>
2019-09-23 21:33:51 -0500

    Parallelization done

--- bench/Monomial.hs
+++ bench/Monomial.hs
@@ -1,54 +1,60 @@
 module Main where
 import Criterion.Main
 import Prelude as P
-import Numeric.Algebra as N
+-- import Numeric.Algebra as N
 import Polynomial.Monomial
-import Control.DeepSeq
+--import Control.DeepSeq
 import Polynomial.Wu
-import Data.List as L
+--import Data.List as L
 import Polynomial.Polynomial
 import Polynomial.Terms
 
 
 main :: IO ()
 main = do
-  let a = 1000
-      b = 1000000
-      c1 = [Poly[Term(1,m[6])], Poly[Term(4,m[3])], Poly[Term(6,m[5])], Poly[Term(10,m[3])]] :: [Poly Rational Revlex]
-      c2 = [Poly[Term(1, mp[1,2][2,1]), Term(2, m[3])], Poly[Term(5,mp[2][5])], Poly[Term(3,mp[1,2][1,1])]] :: [Poly Rational Revlex]
-      c3 = [Poly[Term(3, mp[3][2]), Term(4, mp[1][2])], Poly[Term(3, mp[3][1])]] :: [Poly Rational Revlex]
-      ps = concat $ c1:c2:c3:[]
-     -- ps1 = 
+  let c1 =
+        [ Poly [Term (1, m [6])]
+        , Poly [Term (4, m [3])]
+        , Poly [Term (6, m [5])]
+        , Poly [Term (10, m [3])]
+        ] :: [Poly Rational Revlex]
+      c2 =
+        [ Poly [Term (1, mp [1, 2] [2, 1]), Term (2, m [3])]
+        , Poly [Term (5, mp [2] [5])]
+        , Poly [Term (3, mp [1, 2] [1, 1])]
+        ] :: [Poly Rational Revlex]
+      c3 =
+        [ Poly [Term (3, mp [3] [2]), Term (4, mp [1] [2])]
+        , Poly [Term (3, mp [3] [1])]
+        ] :: [Poly Rational Revlex]
+      ps = concat $ c1 : c2 : c3 : []
+      ps1 =
+        [ Poly [Term (1, mp [2] [2]), Term (-1, m [1, 1]), Term (-1, m [0])] :: Poly Rational Revlex
+        , Poly [Term (1, m [2]), Term (-2, mp [3] [1])] :: Poly Rational Revlex
+        , Poly [Term (1, mp [3] [2]), Term (-1, m [1, 1]), Term (1, m [0])] :: Poly Rational Revlex
+        , Poly [Term (3, mp [2] [5])] :: Poly Rational Revlex
+        ]
+     -- ps1 =
   defaultMain
-
-    [
-      -- bgroup
-      -- "Multiplication"
-      -- [bgroup
-      --   "MonomialM"
-      --   [bench "whnf" $ whnf ((N.*) x) y
-      --   ,bench "nf" $ nf force (((N.*) x) y)
-      --   ,bench "nf-force" $ nf force (((N.*) x) y)
-      --   ]
-      -- ,bgroup
-      --   "MonomialT"
-      --   [bench  "nf" $ nf ((zipWith (P.+)) z) p --260ns
-      --   ,bench  "nf-force" $ nf force (((zipWith (P.+)) z) p) --79ns
-      --   ,bench "whnf" $ whnf ((zipWith (P.+)) z) p ]
-      -- ]
-      bgroup
-      "Polynomial Division"
-      [bgroup
-       "laia"
-       [bench "whnf" $ whnf (laia a) b
-       ,bench "whnf" $ whnf (laia' a) b
-       ,bench "whnf" $ whnf (laia'') b
-       ,bench "whnf repliW" $ whnf (laiaReplicateWork a) b
-       --,bench "nf" $ nf (laiaReplicateWork a ) b 
-       ,bench "whnf" $ whnf (\x -> L.foldl' (P.+) 0 [0 .. x]) b
-       ,bench "nf" $ nf (\x -> L.foldl' (P.+) 0 [0 .. x]) b
-       ]
-      ]
+      -- bgroup "Multiplication" [bgroup "MonomialM" [bench "whnf" $ whnf ((N.*)
+      -- x) y ,bench "nf" $ nf force (((N.*) x) y) ,bench "nf-force" $ nf force
+      -- (((N.*) x) y) ] ,bgroup "MonomialT" [bench "nf" $ nf ((zipWith (P.+))
+      -- z) p --260ns ,bench "nf-force" $ nf force (((zipWith (P.+)) z) p)
+      -- --79ns ,bench "whnf" $ whnf ((zipWith (P.+)) z) p ] ]
+    [ bgroup
+        "Normal division"
+        [bench "whnf" $ whnf (psByTf ps) ps1, bench "nf" $ nf (psByTf ps) ps1]
+    , bgroup
+        "IO division"
+        [ bench "whnf" $ whnf (psByTfLRIO ps) ps1
+        , bench "nf" $ nf (psByTfLRIO ps) ps1
+        ]
+    , bgroup
+        "[] division"
+        [ bench "whnf" $ whnf (psByTfLR ps) ps1
+        , bench "nf" $ nf (psByTfLR ps) ps1
+        ]
+    , bgroup
+        "wrapper"
+        [bench "whnf" $ whnf (wrapper ps) ps1, bench "nf" $ nf (wrapper ps) ps1]
     ]
-
-
--- src/Polynomial/Wu.hs
+++ src/Polynomial/Wu.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE IncoherentInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 module Polynomial.Wu (
-                     laia,
-                     laia',
-                     laia'',
+psByTf,
+psByTfLR,
+psByTfLRIO,
+wrapper
                      ) where
 import Control.Scheduler
 import Polynomial.Monomial
@@ -16,6 +18,7 @@ import Numeric.Algebra as N
 import Prelude as P
 import Control.Concurrent
 import Control.DeepSeq
+import Foreign.Marshal.Unsafe
 --import Numeric.Algebra as N
 -- import Prelude as P
 -- Group the polynomial set PS in different classes
@@ -81,10 +84,10 @@ h xs = [ fst x |x <- xs]
 -----------------------------------------------------------------------------
 ----------------------------------------------------------------------------
 -- take a basic set using the classification class 
-basicSet :: (Num t, Eq t) => [Poly t Revlex] -> [Poly t Revlex]
+basicSet :: [Poly Rational Revlex] -> [Poly Rational Revlex]
 basicSet xs =  sPLRR (setClassPs xs) []
 -----------------------------------------------------------------------------
-charSet :: (Num t, Eq t)=>[Poly t Revlex] -> [Poly t Revlex]
+charSet :: [Poly Rational Revlex] -> [Poly Rational Revlex]
 charSet ps
   | basicSet ps /= ps  = charSet a
   | otherwise = ps
@@ -92,7 +95,7 @@ charSet ps
     a = bsDividePs ps  (basicSet ps)
 -----------------------------------------------------------------------------
 -- division of every pk by Bs in PS
-bsDividePs :: (Num t, Eq t)=>[Poly t Revlex] -> [Poly t Revlex] -> [Poly t Revlex]
+bsDividePs ::(Num t, Eq t) =>[Poly t Revlex] -> [Poly t Revlex] -> [Poly t Revlex]
 bsDividePs ps bs = psByTf (red bs ps) bs ++ bs 
 -----------------------------------------------------------------------------
 -- auxiliar function that perform the division of each polynomial by the triangular form
@@ -102,49 +105,45 @@ psByTf ps bs = [ sprem x bs | x <- ps]
 ----- *****************************-----------------------
 -- function that paralellism the work
 --poly set by  triangular form lehins recomendation
-psByTfLR :: [Poly Rational Revlex] -> [Poly Rational Revlex] -> IO [Poly Rational Revlex]
-psByTfLR ps bs = traverseConcurrently Par (\p -> p `deepseq` pure p) [sprem  x bs | x <- ps] :: IO[Poly Rational Revlex]
+psByTfLR ::  [Poly Rational Revlex] -> [Poly Rational Revlex] -> [Poly Rational Revlex]
+psByTfLR ps bs =  unsafeLocalState ( traverseConcurrently Par (\p -> p `deepseq` pure p) [sprem  x bs | x <- ps] :: IO [Poly Rational Revlex])
 
+wrapper ::  [Poly Rational Revlex] -> [Poly Rational Revlex] -> [Poly Rational Revlex]
+wrapper ps bs = unsafeLocalState $ psByTfLRIO ps bs 
 
+psByTfLRIO ::  [Poly Rational Revlex] -> [Poly Rational Revlex] -> IO[Poly Rational Revlex]
+psByTfLRIO ps bs = traverseConcurrently Par (\p -> p `deepseq` pure p) [sprem  x bs | x <- ps] :: IO [Poly Rational Revlex]
+
+instance NFData (IO[Poly Rational Revlex]) where
+  rnf x = seq x ()
 -- psByTfPM :: (Num t, Eq t) =>  [Poly t Revlex] -> [Poly t Revlex]-> IO [Poly t Revlex]
 -- psByTfPM ps bs = withScheduler Par $ \x -> do
 --   p <- ps 
 --   scheduleWork x $ pure (sprem p bs )
 
-psByTfP :: (Num t, Eq t) => [Poly t Revlex] -> [Poly t Revlex]-> IO [[Poly t Revlex]]
-psByTfP ps bs = withScheduler Par $ \x -> scheduleWork x $ pure ([(sprem x) bs | x <- ps]) -- paralelisa?
+--psByTfP :: (Num t, Eq t) => [Poly t Revlex] -> [Poly t Revlex]-> IO [[Poly t Revlex]]
+--psByTfP ps bs = withScheduler Par $ \x -> scheduleWork x $ pure ([(sprem x) bs | x <- ps]) -- paralelisa?
 
-psByTfPR :: (Num t, Eq t) => [Poly t Revlex] -> [Poly t Revlex]-> IO [[Poly t Revlex]]
-psByTfPR ps bs = withScheduler Par $ \x -> replicateM (length ps) $ scheduleWork x $ pure ([(sprem x) bs | x <- ps]) -- replica one single work 
+--psByTfPR :: (Num t, Eq t) => [Poly t Revlex] -> [Poly t Revlex]-> IO [[Poly t Revlex]]
+--psByTfPR ps bs = withScheduler Par $ \x -> replicateM (length ps) $ scheduleWork x $ pure ([(sprem x) bs | x <- ps]) -- replica one single work 
 
-psByTfPS :: (Num t, Eq t) => [Poly t Revlex] -> [Poly t Revlex]-> IO [Poly t Revlex]
-psByTfPS ps bs = withScheduler Par $ \x -> do scheduleWork x $ pure ( sprem (ps !! 0) bs )
-                                              scheduleWork x $ pure ( sprem (ps !! 1) bs ) -- there is no way we can know the length of a set
+--psByTfPS :: (Num t, Eq t) => [Poly t Revlex] -> [Poly t Revlex]-> IO [Poly t Revlex]
+--psByTfPS ps bs = withScheduler Par $ \x -> do scheduleWork x $ pure ( sprem (ps !! 0) bs )
+--                                              scheduleWork x $ pure ( sprem (ps !! 1) bs ) -- there is no way we can know the length of a set
 -- como crear funciones de acuerdo al input
 
-laia :: Int -> Int -> IO [Int]
-laia a b= withScheduler Par $ \x -> replicateM_ a $ scheduleWork  x  $ pure (L.foldl' (P.+) 0 [0 .. b]) -- scheduleId x
+-- laia :: Int -> Int -> IO [Int]
+-- laia a b= withScheduler Par $ \x -> replicateM_ a $ scheduleWork  x  $ pure (L.foldl' (P.+) 0 [0 .. b]) -- scheduleId x
 
-laia' :: Int -> Int -> IO[Int]
-laia' a b = withScheduler Par $ \x -> do replicateM a $ scheduleWork  x  $ pure (L.foldl' (P.+) 0 [0 .. b]) -- scheduleId x
--- scheduleId = (`scheduleWorkId` (\ i -> threadDelay 1000000 >> print i >> pure (L.foldl' (P.+) 0 [0 .. 100000] )))
+-- laia' :: Int -> Int -> IO[Int]
+-- laia' a b = withScheduler Par $ \x -> do replicateM a $ scheduleWork  x  $ pure (L.foldl' (P.+) 0 [0 .. b]) -- scheduleId x
+-- -- scheduleId = (`scheduleWorkId` (\ i -> threadDelay 1000000 >> print i >> pure (L.foldl' (P.+) 0 [0 .. 100000] )))
 
-laia'' :: Int -> IO [Int]
-laia'' a =
-  withScheduler Par $ \x -> do
-    scheduleWork x $ pure (L.foldl' (P.+) 0 [0 .. div a 2])
-    scheduleWork x $ pure (L.foldl' (P.+) 0 [div a 2  .. a])
-
-getM :: Int -> IO Int
-getM x = pure x
-
-nine :: Int -> IO[Int]
-nine x = undefined --replicateM x getM
-
-p1 :: Poly Rational Revlex
-p1 = Poly[Term(2,m[2])]
-p2 :: Poly Rational Revlex
-p2 = Poly [Term(2,m[2])]
+-- laia'' :: Int -> IO [Int]
+-- laia'' a =
+--   withScheduler Par $ \x -> do
+--     scheduleWork x $ pure (L.foldl' (P.+) 0 [0 .. div a 2])
+--     scheduleWork x $ pure (L.foldl' (P.+) 0 [div a 2  .. a])
 ---------------- ************** -----------------------------
 -- sucesive pseudo division of a polynomial and a polynomial set
 sprem :: (Num t, Eq t) => Poly t Revlex -> [Poly t Revlex] -> Poly t Revlex
@@ -160,9 +159,6 @@ red :: (Eq t) => [Poly t Revlex] -> [Poly t Revlex] -> [Poly t Revlex]
 red [] xp = xp
 red (x:xs) xp = red xs (delete x xp)
 -----------------------------------------------------------------------------
-
-
-
 -- graphics calculator examples
 -- p1 = Poly [Term(1,mp[2][2]), Term(-1,m[1,1]), Term(-1,m[0])] :: Poly Rational Revlex
 -- p2 = Poly [Term(1,m[2]), Term(-2,mp[3][1])]:: Poly Rational Revlex
@@ -184,10 +180,4 @@ red (x:xs) xp = red xs (delete x xp)
 -- f1 = Poly[Term(1,mp[2][2]), Term(-1, m[1,1]), Term(-1, m[0])] :: Poly Rational Revlex; f2 = Poly[Term(1,m[2]), Term(-2,mp[3][1])] :: Poly Rational Revlex; f3 = Poly[Term(1,mp[3][2]), Term(-1,m[1,1]), Term(1,m[0])] :: Poly Rational Revlex
 -- c1 = [Poly[Term(1,m[6])], Poly[Term(4,m[3])], Poly[Term(6,m[5])], Poly[Term(10,m[3])]] :: [Poly Rational Revlex]; c2 = [Poly[Term(1, mp[1,2][2,1]), Term(2, m[3])], Poly[Term(5,mp[2][5])], Poly[Term(3,mp[1,2][1,1])]] :: [Poly Rational Revlex]; c3 = [Poly[Term(3, mp[3][2]), Term(4, mp[1][2])], Poly[Term(3, mp[3][1])]] :: [Poly Rational Revlex]; ps = concat $ c1:c2:c3:[]
 
-getLine' :: Maybe Int
-getLine' = Just 5
 
-hello :: Maybe String
-hello = do
-  name <- getLine'
-  Just $ show name ++ "ose"
